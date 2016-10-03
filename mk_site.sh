646cd5d8eaee1348d4554516f10c9f951c7acb12
fennecdjay <astor.jeremie@wanadoo.fr>
2016-10-04 01:07:21 +0200

    remove make script from _site

--- mk_site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf _site
-git clone -b gh-pages `git config remote.origin.url` _site
-bundle exec jekyll b
-cd _site
-git add -A
-git commit -am 'Yeah. Built from subdir'
-git push
-cd ..
