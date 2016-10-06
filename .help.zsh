ce18b7fc814b1ceb2d3faf98084f99a4aa37e7dd
fennecdjay <astor.jeremie@wanadoo.fr>
2016-10-07 00:00:03 +0200

    ...

---
+++ .help.zsh
@@ -0,0 +1,9 @@
+toilet Gwion-site --metal
+echo "
+You can \033[31mrun\033[0m the site \033[31mlocally\033[0m with:
+	\033[32m./site.sh\033[0m \033[1mrun\033[0m
+or \033[31mdeploy\033[0m it with:
+	\033[32m./site.sh\033[0m \033[1mdeploy\033[0m
+You can \033[31madd a post\033[0m with:
+	\033[32m./site.sh\033[0m \033[1m<category> <title>\033[0m
+"
